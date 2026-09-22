56b2be997f92fa207303e1638787bf7dd56f2bbb
Shahed Dwaik <shaheddwaik172003@gmail.com>
2026-09-22 19:48:36 +0300

    my first App in Flutter

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,18 +1,22 @@
-import 'package:flutter/material.dart';
-import 'package:first_app/style_text.dart';
+/*import 'package:flutter/material.dart';
+//import 'package:first_app/style_text.dart';
 
 const  startAlignment = Alignment.topLeft;
 const endAlignment = Alignment.bottomRight;
 
-var color1 = const Color.fromARGB(255, 203, 18, 31);
+var color1 = const Color.fromARGB(255, 0, 0, 0);
 var color2 = const Color.fromARGB(255, 249, 249, 249);
 
 
 class GradientContainer extends StatelessWidget {
-  const GradientContainer( this.colors ,{super.key});
+ GradientContainer( this.colors ,{super.key});
   
   final List<Color> colors;
-
+  var activeDiceImage = 'assets/images/dice-2.png';
+void rollDice() {
+  activeDiceImage = 'assets/images/dice-4.png';
+  print('changing image .....');
+}
 
   @override
   Widget build(BuildContext context) {
@@ -24,7 +28,25 @@ class GradientContainer extends StatelessWidget {
               end: endAlignment
               )
           ),
-          child: const Center(child: StyleText("Hello Duduu ")),
+          child:  Center(
+            child:Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+            Image.asset(
+             activeDiceImage,width: 200,height: 200,),
+              const SizedBox(height: 20,),
+               TextButton(
+                onPressed: () {
+                 rollDice();
+                },
+                style: TextButton.styleFrom(
+                 // padding: const EdgeInsets.only(top: 20),
+                  foregroundColor: Colors.white,
+                  textStyle: const TextStyle(fontSize: 28),
+                ),
+                child: const Text('Roll Dice')),
+          ],)
+          ),
       );
   }
-}
+}*/
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,41 @@
-import 'package:flutter/material.dart';
-import 'package:first_app/gradient_container.dart';
+import 'package:flutter/material.dart'; // have to import this package to use the material design widgets
+//import 'package:first_app/gradient_container.dart';
 
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter Demo Home Page' , style: TextStyle(fontSize: 24, color: Color.fromARGB(255, 183, 13, 13))),
+        ),
+        backgroundColor: const Color.fromARGB(255, 200, 106, 137),
+        body:  Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Hello World!', style: TextStyle(fontSize: 24, 
+              color: Color.fromARGB(255, 7, 210, 68))),
+              SizedBox(height: 20),
+              Text('Welcome to my first Flutter app!', style: TextStyle(fontSize: 18, 
+              color: Color.fromARGB(255, 20, 105, 157))),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/*
 void main() {
   runApp(
     MaterialApp(// the root widget of your application
@@ -9,6 +44,6 @@ void main() {
         body: GradientContainer([color1, color2]),
     ),
   ));
-}
+}*/
 
 
